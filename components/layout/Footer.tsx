19b3459491b95f2d22f8a31b922e174066064f7d
fabian-roeben <fabian.roeben@web.de>
2025-01-01 12:54:52 +0100

    Make year dynamic to current year

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,11 +1,13 @@
 export function Footer() {
-    return (
-      <footer className="border-t">
-        <div className="container mx-auto pl-4 lg:pl-16 py-4 max-w-6xl">
-          <p className="text-muted-foreground text-xs">
-            © 2024 Fabian Roeben
-          </p>
-        </div>
-      </footer>
-    )
-  }
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="border-t">
+      <div className="container mx-auto pl-4 lg:pl-16 py-4 max-w-6xl">
+        <p className="text-muted-foreground text-xs">
+          © {currentYear} Fabian Roeben
+        </p>
+      </div>
+    </footer>
+  )
+}
