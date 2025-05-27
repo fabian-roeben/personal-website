b21c1e86fc4d60349cbde547e685138610b0d11a
fabian-roeben <fabian.roeben@web.de>
2025-05-27 08:43:08 +0200

    Remove paper status, modify papers

--- data/papers.ts
+++ data/papers.ts
@@ -16,31 +16,62 @@ export const papers: Paper[] = [
   // },
   {
     title: "Macroeconomic Expectations and Household Types: Evidence from Panel Data",
-    status: "Work in Progress",
     abstract: "Using expectations panel data of 11,302 US households, I analyze households' subjective models of macroeconomic outcomes and show that those models differentially affect planned consumption. To do this, I first use cluster analysis to classify households into types based on the interdependence of their expectations of four key macroeconomic variables: inflation, unemployment, interest rates, and stock prices. I find that households can be clustered into five types, of which three can be interpreted meaningfully: the first forms expectations consistent with economic concepts and the second consistent with a simple affective heuristic. The third seems to form expectations in isolation without interrelating variables. Second, I investigate the behavioral relevance of belonging to a type by analyzing the impact of households' inflation expectations on planned consumption. I find that the first and third - consistent with intertemporal substitution - increase planned spending with an increase in inflation expectations. The second - consistent with an income effect or precautionary saving - decreases planned spending. This research has policy implications for monetary and fiscal authorities seeking to influence household consumption patterns through policy measures.",
     links: [
       { name: "Data", url: "https://www.newyorkfed.org/microeconomics/sce#/" }
     ]
   },
+  // {
+  //   title: "Beyond the Click: Inverting Online Behavior to Preferences",
+  //   authors: [
+  //     { name: "Vanessa Sticher", website: "https://economics.mit.edu/people/phd-students/vanessa-sticher"},
+  //   ],
+  // },
   {
-    title: "AI Customization and the Market for News",
+    title: "News Customization with AI",
     authors: [
       { name: "Felix Chopra", website: "https://www.felixchopra.com/" },
       { name: "Ingar Haaland", website: "https://sites.google.com/site/ingarhaaland/" },
       { name: "Chris Roth", website: "https://sites.google.com/site/chrisrotheconomics" },
       { name: "Vanessa Sticher", website: "https://economics.mit.edu/people/phd-students/vanessa-sticher"},
     ],
-    status: "Work in Progress",
     links: [
       { name: "Pre-registration", url: "https://www.socialscienceregistry.org/trials/14561"}
     ]
   },
   {
+    title: "The Asymmetric Response to Political Spin",
+    authors: [
+      { name: "Felix Chopra", website: "https://www.felixchopra.com/" },
+      { name: "Ingar Haaland", website: "https://sites.google.com/site/ingarhaaland/" },
+      { name: "Chris Roth", website: "https://sites.google.com/site/chrisrotheconomics" },
+      { name: "Vanessa Sticher", website: "https://economics.mit.edu/people/phd-students/vanessa-sticher"},
+    ],
+  },
+  // {
+  //   title: "Motivational Interviews at Scale",
+  //   authors: [
+  //     { name: "Felix Chopra", website: "https://www.felixchopra.com/" },
+  //     { name: "Ingar Haaland", website: "https://sites.google.com/site/ingarhaaland/" },
+  //     { name: "Nicolas Röver", website: "https://sites.google.com/view/nicolasroever/"},
+  //     { name: "Chris Roth", website: "https://sites.google.com/site/chrisrotheconomics" },
+  //   ],
+  // },
+  // {
+  //   title: "Political Correctness and Polarization",
+  //   authors: [
+  //     { name: "Felix Chopra", website: "https://www.felixchopra.com/" },
+  //     { name: "Ingar Haaland", website: "https://sites.google.com/site/ingarhaaland/" },
+  //     { name: "Chris Roth", website: "https://sites.google.com/site/chrisrotheconomics" },
+  //     { name: "Vanessa Sticher", website: "https://economics.mit.edu/people/phd-students/vanessa-sticher"},
+  //     { name: "Matthias Sutter", website: "https://www.mpg.de/11389045/research-collective-goods-sutter"},
+  //   ],
+  // },
+  {
     title: "Unraveling Sovereign Risk: Narrative Evidence from Earnings Calls",
     authors: [
       { name: "Nicolas Röver", website: "https://sites.google.com/view/nicolasroever/" },
     ],
-    status: "Work in Progress",
   },
   {
     title: "Digging Deeper into The Impact of Capital Flow Management: Evidence from A New Dataset",
@@ -48,6 +79,5 @@ export const papers: Paper[] = [
       { name: "Dimitre Milkov" },
       { name: "Niamh Sheridan" }
     ],
-    status: "Work in Progress",
   },
 ];
